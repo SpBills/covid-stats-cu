d187a3d6ef972ff1c7e0954024b9af673c5eea94
spbills <spencebills0@gmail.com>
2021-01-18 11:50:21 -0500

    Added framework.

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,16 @@
-fn main() {
+use actix_web::{App, HttpServer};
+use cronjob::CronJob;
+
+mod tasks;
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    let mut cron = CronJob::new("Fetch COVID data.", tasks::polling::CovidData::poll);
+    cron.minutes("5");
+    cron.start_job();
+
+    HttpServer::new(|| App::new())
+        .bind("localhost:9090")?
+        .run()
+        .await
 }
---
+++ src/tasks/polling.rs
@@ -0,0 +1,11 @@
+pub struct CovidData {
+    total_cases: i32,
+    on_campus: i32,
+    off_campus: i32,
+}
+
+impl CovidData {
+    pub fn poll(_s: &str) {
+
+    }
+}
